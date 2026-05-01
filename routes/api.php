e0d3cd493af1092613c1069fac3d44a8f01495f6
leicer tovar chiripua <leithtovarch0619@gmail.com>
2026-04-30 19:36:08 -0500

    configuracion de cors para union del frontend y el backend

--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,12 @@ Route::group([
     Route::post('/registrer', [AuthController::class, 'registrer'])->name('auth.registrer');
     Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
     Route::post('/refresh', [AuthController::class, 'refresh'])->name('auth.refresh');
+
+    // Nuevas rutas protegidas
+    Route::middleware('auth:api')->group(function () {
+
+        Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/me',     [AuthController::class, 'me']);
+    });
     
 });
