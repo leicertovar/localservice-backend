25ed00fdd5888082083dcd5e4e475a007ad8f157
leicer tovar chiripua <leithtovarch0619@gmail.com>
2026-05-24 21:46:04 -0500

    cambios en el cors a permitir url de produccion

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,8 @@
 return [
     'paths'             => ['api/*'],
     'allowed_methods'   => ['*'],
-    'allowed_origins'   => ['http://localhost:8080'],
+   // 'allowed_origins'   => ['http://localhost:8080'],
+   'allowed_origins'   => ['http://localservice.ds2.eleueleo.com'],
     'allowed_headers'   => ['*'],
     'exposed_headers'  => [],
     'max_age'          => 0,
